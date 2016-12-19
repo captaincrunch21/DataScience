2ca590df8a7a11ae5573899e3695fe0b62ceb01c
captaincrunch21 <saiasdfgh@gmail.com>
2016-12-19 11:28:35 -0800

    completed

--- Nearestneighbours.py
+++ Nearestneighbours.py
@@ -1,28 +1,67 @@
 from __builtin__ import int
+from operator import itemgetter
+
 
 import prereq
 from prereq import Correctness
 from math import  *
+
 def distance_points(x,y,x1,y1):
     t =((x-x1)^2 + (y-y1)^2 )
     try :
         return sqrt(t)
     except :
         return 0
+
+def sorter(list):
+    for i  in range(len(list)):
+        for j in range(len(list)):
+            if list[i][0] < list[j][0]:
+                t=list[j]
+                list[j]=list[i]
+                list[i]=t
+
+    return list
+
+
+
+
+
+def answer(list):
+    dict={}
+    for data in list:
+        try:
+            dict[data[1]]
+            dict[data[1]]=dict[data[1]] + 1
+        except:
+            dict[data[1]] = 0
+    data=[0,'']
+
+    for label,count in dict.iteritems():
+
+        if count > data[0] :
+            data[0] = count
+            data[1] = label
+
+
+
+    return data[1]
+
+
+
 def ktestmodel(k,data_train,data):
     distance = []
+
     for datat in data_train:
         temp= distance_points(datat[0],datat[1],data[0],data[1])
 
-        if len(distance) < 3 :
-            t = zip(temp,datat[2])
-            distance.append(t)
-        else:
-            md=0.0
-            mlabel=""
-            for i in range(k):
-                d,label = zip(*distanc[i])
-                if
+        distance.append([temp,datat[2]])
+    distance=sorter(distance)
+
+
+
+    distance=distance[0:k]
+    return answer(distance)
 
 
 
@@ -53,7 +92,18 @@ k = int(raw_input("Number of nearest nodes"))
 
 data_train=zip(x_train,y_train,label_train)
 data_test=zip(x_test,y_test,label_test)
-for data in data_test:
-    ktestmodel(k,data_train,data)
+t=0
+f=0
 
+
+for data in data_test:
+    label=ktestmodel(k,data_train,data)
+    print label,data[2]
+    if label is data[2]:
+        t= t + 1
+    else:
+        f =f +1
+
+print t
+print f
 
